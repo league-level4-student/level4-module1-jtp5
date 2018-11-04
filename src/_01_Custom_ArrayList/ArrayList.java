85c4a3a56ff11b34b20c7eb80e03f167b865b49b
11000783 <josephaguilar2022@gmail.com>
2018-11-04 14:24:02 -0800

    Finished CustomArray

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -4,32 +4,63 @@ package _01_Custom_ArrayList;
 
 public class ArrayList <T>{
 	
+	private T[] list;
+	
 	public ArrayList() {
+		list = ((T[])new Object[0]);
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return list[loc];
 	}
 	
 	public void add(T val) {
-		
+		T[] copy = ((T[])new Object[list.length + 1]);
+		for (int i = 0; i < list.length; i++) {
+			copy[i] = list[i];
+		}
+		copy[copy.length - 1] = val;
+		list = copy;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] copy = ((T[])new Object[list.length + 1]);
+		copy[loc] = val;
+		for (int i = 0; i < loc; i++) {
+			copy[i] = list[i];
+		}
+		for (int i = loc; i < list.length; i++) {
+			copy[i + 1] = list[i];
+		}
+		list = copy;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] copy = ((T[])new Object[list.length - 1]);
+		for (int i = 0; i < loc; i++) {
+			copy[i] = list[i];
+		}
+		for (int i = loc; i < copy.length; i++) {
+			copy[i] = list[i+1];
+		}
+		list = copy;
 	}
 	
 	public boolean contains(T val) {
-		
+		for (int i = 0; i < list.length; i++) {
+			if(list[i] == val) {
+				return true;
+			}
+		}
 		return false;
+	}
+	
+	public int size() {
+		return list.length;
 	}
 }
