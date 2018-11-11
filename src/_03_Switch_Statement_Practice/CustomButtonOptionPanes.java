c2f103a7e2054a24b1f811b60a048e47fee9b010
jtp5 <league@WTS22.attlocal.net>
2018-11-11 14:24:54 -0800

    finished everything but snake

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -4,14 +4,36 @@ import javax.swing.JOptionPane;
 
 public class CustomButtonOptionPanes {
 	public static void main(String[] args) {
-		String[] options = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		String[] options = { "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday", "Sunday" };
 
 		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
 				options, 0);
 
 		String choice = options[input];
-		
-		//use a switch statement to do something cool for each option
-		
+
+		// use a switch statement to do something cool for each option
+		switch (choice) {
+		case "Sunday":
+			System.out.println("That's Today!");
+			break;
+		case "Monday":
+			System.out.println("Ew Mondays suck.");
+			break;
+		case "Tuesday":
+			System.out.println("Tuesday's aight.");
+			break;
+		case "Wednesday":
+			System.out.println("Hump day!");
+			break;
+		case "Thursday":
+			System.out.println("It's the home stretch");
+			break;
+		case "Friday":
+			System.out.println("FINALLY!!");
+			break;
+		case "Saturday":
+			System.out.println("EZ Clap");
+			break;
+		}
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -3,12 +3,57 @@ package _05_Enum_Stuff;
 import javax.swing.JOptionPane;
 
 public class _00_Horoscope {
-	// 1. Create an enum in a separate file called Zodiac that contains a category for
-	//    all 12 zodiac signs.
-	
-	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
-	//    a different horoscope based on the Zodiac's state.
-	
+	// 1. Create an enum in a separate file called Zodiac that contains a category
+	// for
+	// all 12 zodiac signs.
+
+	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane
+	// to display
+	// a different horoscope based on the Zodiac's state.
+	public static void horoscope(Zodiac z) {
+		switch (z) {
+		case ARIES:
+			JOptionPane.showMessageDialog(null, "Horoscopes are dumb but u Aries");
+			break;
+		case TAURUS:
+			JOptionPane.showMessageDialog(null, "Bullsh*t");
+			break;
+		case GEMINI:
+			JOptionPane.showMessageDialog(null, "I remember this from Beyblade");
+			break;
+		case CANCER:
+			JOptionPane.showMessageDialog(null, "Lmao");
+			break;
+		case LEO:
+			JOptionPane.showMessageDialog(null, "I think this one's me");
+			break;
+		case VIRGO:
+			JOptionPane.showMessageDialog(null, "More like virgin pepeD xddd");
+			break;
+		case LIBRA:
+			JOptionPane.showMessageDialog(null, "What do you call an offspring between a library and a zebra?");
+			break;
+		case SCORPIO:
+			JOptionPane.showMessageDialog(null, "This little guy's scary");
+			break;
+		case SAGITTARIUS:
+			JOptionPane.showMessageDialog(null, "I also remember this from Beyblade");
+			break;
+		case CAPRICORN:
+			JOptionPane.showMessageDialog(null, "More like candycorn lul");
+			break;
+		case AQUARIUS:
+			JOptionPane.showMessageDialog(null, "Aquaman?");
+			break;
+		case PISCES:
+			JOptionPane.showMessageDialog(null, "Why tf u saying fish");
+			break;
+		}
+	}
+
 	// 3. Make a main method to test your method
-	
+	public static void main(String[] args) {
+		Zodiac z = Zodiac.PISCES;
+		horoscope(z);
+	}
 }
